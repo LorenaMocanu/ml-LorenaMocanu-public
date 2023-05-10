2a9f08cc750dedfb009ceb93486d8e3dcfd57155
LorenaMocanu <131756439+LorenaMocanu@users.noreply.github.com>
2023-05-10 12:27:44 +0100

    ok

--- helloworld.php
+++ helloworld.php
@@ -1,3 +1,6 @@
+</body>
+</html>
+
 <html>
 
 <body>
@@ -9,15 +12,22 @@
 <?php
 $arrayErrors = array();
 
-// Recibimos los datos enviados a través del formulario
 $namePlace = $_POST["number"];
 $descriptionPlace = $_POST["descriere"];
 $municipioText = $_POST["municipio"];
 $informationUrl = $_POST["url"];
 $infoGoogle = $_POST["gurl"];
-$image = $_POST["input-file"];
 
-// Validamos los datos
+
+// Verificam si incarcam imaginea
+if(isset($_FILES['imagine'])) {
+  $nume_imagine = $_FILES['imagine']['name'];
+  $tmp_imagine = $_FILES['imagine']['tmp_name'];
+  $folder = "imagini/";
+  move_uploaded_file($tmp_imagine, $folder.$nume_imagine);
+}
+
+
 if ($namePlace == "") {
     $arrayErrors[] = "<li>ERROR: The name field is empty";
 }
@@ -33,9 +43,7 @@ if ($informationUrl == "") {
 if ($infoGoogle == "") {
     $arrayErrors[] = "<li>ERROR: The URL (Google Maps) field is empty";
 }
-if ($image == "") {
-    $arrayErrors[] = "<li>ERROR: The image field is empty";
-}
+
 
 if (strlen($namePlace) > 20) {
     $arrayErrors[] = "<li>ERROR: The size of the name field greater than 20 characters";
@@ -49,13 +57,8 @@ if (strlen($informationUrl) > 100) {
 if (strlen($infoGoogle) > 100) {
     $arrayErrors[] = "<li>ERROR: The size of the URL (Google Maps) field greater than 100 characters";
 }
-if (strlen($image) > 100) {
-  $arrayErrors[] = "<li>ERROR: The size of the URL (Google Maps) field greater than 100 characters";
-}
 
 
-
-// Mostramos los diferentes campos
 if (sizeof($arrayErrors) > 0)
 {
     echo "<H2>Detected some errors</H2>";    
@@ -74,12 +77,15 @@ else
     echo "<li>The description of the place is...$descriptionPlace";
     echo "<li>The URL with info of the place is...$informationUrl";
     echo "<li>The URL with the map of the place is...$infoGoogle";
-    echo "<li>The image name is...$image";
+    if(isset($nume_imagine)) {
+      echo "<p><img src='imagini/$nume_imagine'></p>";
+    }
+    
     echo "</ul>";
 }
 
 ?>
-<a href="formula.html">Volver al formulario</a>
+<button><a href="formula.html">Volver al formulario</button></a>
 </body>
 
 </html>
