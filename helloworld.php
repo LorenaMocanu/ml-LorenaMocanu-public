2ac7656c7d78e35df30fe6246d7bb2d902750583
LorenaMocanu <131756439+LorenaMocanu@users.noreply.github.com>
2023-05-10 10:34:37 +0100

    K

--- helloworld.php
+++ helloworld.php
@@ -15,7 +15,7 @@ $descriptionPlace = $_POST["descriere"];
 $municipioText = $_POST["municipio"];
 $informationUrl = $_POST["url"];
 $infoGoogle = $_POST["gurl"];
-$image = $_FILES["input-file"];
+$image = $_POST["input-file"];
 
 // Validamos los datos
 if ($namePlace == "") {
